2cb20c663b680687258075bcf5cc4c17124b5914
Omer H <omer.h1910@gmail.com>
2023-05-10 01:32:08 +0300

    first actual commit

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,19 @@
+import "../css/pages/MainPage.css";
+
+function MainPage() {
+  return (
+    <>
+      <div className="MainPage">
+        <div className="MainPage__title">
+          <h1>GONEXT</h1>
+          <h3>.GG</h3>
+        </div>
+        <div className="MainPage__searchBox">
+          <input placeholder="Summoner Name"></input>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainPage;
